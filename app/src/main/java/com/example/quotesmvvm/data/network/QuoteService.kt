919a88e475667e35718eba0418dda1e42e39c402
Fabio Pereira <fabiodev587@gmail.com>
2024-04-12 17:30:28 -0300

    QuoteService

--- app/src/main/java/com/example/quotesmvvm/data/network/QuoteService.kt
+++ app/src/main/java/com/example/quotesmvvm/data/network/QuoteService.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.withContext
 class QuoteService {
     private val retrofit = RetrofitHelper.getRetrofit()
 
+
     suspend fun getQuotes(): List<QuoteModel> {
         val response = retrofit.create(QuoteApiClient::class.java).getAllQuotes()
         return withContext(Dispatchers.IO) {
